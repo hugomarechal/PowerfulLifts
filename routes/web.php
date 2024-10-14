978c2b7510e9a7cf976ccc2758ee291cedfeb818
hugo <hugo_m7@live.fr>
2024-10-15 00:42:41 +0200

    refacto

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-//guest routes
+Route::middleware('guest')->group(function() {
+    Route::get('/signup', function () {
+        return Inertia::render('SignUp');
+    });
+    Route::post('signup', [\App\Http\Controllers\UserController::class, 'signup'])->name('signup');
 
-Route::get('/signup', function () {
-    return Inertia::render('SignUp');
+    Route::get('/login', function () {
+        return Inertia::render('Login');
+    });
+
+    Route::post('login', [\App\Http\Controllers\UserController::class, 'login'])->name('login');
 });
-Route::post('signup', [\App\Http\Controllers\UserController::class, 'signup'])->name('signup');
 
-Route::get('/login', function () {
-    return Inertia::render('Login');
-});
 
-Route::post('login', [\App\Http\Controllers\UserController::class, 'login'])->name('login');
-
-//auth routes
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard', function () {
         return Inertia::render('BaseDashboard');
